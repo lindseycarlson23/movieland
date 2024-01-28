04646c0e8ee7ae8186f2aa262414a7f09256c08c
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2024-01-28 17:27:33 -0600

    added dynamic search

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,9 @@ import MovieCard from "./MovieCard";
 
 const apiUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=a4b8a086";
 
-const movie1 = {
-  "Title": "The Greatest Showman",
-  "Year": "2017",
-  "imdbID": "tt1485796",
-  "Type": "movie",
-  "Poster":
-    "https://m.media-amazon.com/images/M/MV5BMjI1NDYzNzY2Ml5BMl5BanBnXkFtZTgwODQwODczNTM@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${apiUrl}&s=${title}`);
@@ -34,27 +26,27 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="showman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
-
-      {movies.length > 0
-      ? (
+      {movies.length > 0 ? (
         <div className="container">
-        {movies.map((movie) => (
-          <MovieCard movie={movie}/>
-        ))}
-      </div>
-      ) :
-      (
-        <div className="empty">
-        <h2>No movies found</h2>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
-      )
-    }
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </>
   );
 };
