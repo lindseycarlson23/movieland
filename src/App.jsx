410e04de728b5b454f7c7a5d3ffefdf2de3be755
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2024-01-28 16:34:30 -0600

    removed extra css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import SearchIcon from './assets/search.svg'
+import SearchIcon from "./assets/search.svg";
+// import Normalize from "react-normalize";
+// import React, { Fragment } from "react";
 
 // a4b8a086
 // http://www.omdbapi.com/?i=tt3896198&apikey=a4b8a086
@@ -11,17 +13,18 @@ const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
-    console.log(data);
-  }
+    console.log(data.Search);
+  };
   useEffect(() => {
     searchMovies("showman");
   }, []);
 
   return (
     <>
-      <div className='app'>
+      <div className="app">
         <h1>MovieLand</h1>
       </div>
+      <div className=""></div>{" "}
     </>
   );
 };
